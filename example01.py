46aef7199d0ee0cdf689014fe007b6c04e8ebd67
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2019-06-05 14:30:27 -0500

    Add files via upload

---
+++ example01.py
@@ -0,0 +1,16 @@
+"""
+NOMBRE:Fernando Hernandez Vazquez
+ID:1719210003
+DATE:05/06/2019
+GROUP:TIC22
+DESCRIPTION:Casting
+"""
+number_01=10.5
+number_02=0
+if number_01<= number_02:
+	add=number_01+number_02
+	print ("Add"), str (add)
+else:
+	division=number_01/ number_02
+	print division
+	print "end"
