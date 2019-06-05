984ec2f1b7027a1b0df5d9a7ce70d55986a210bb
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2019-06-05 14:54:42 -0500

    Add files via upload

---
+++ pi.py
@@ -0,0 +1,15 @@
+"""
+NOMBRE:Fernando Hernandez Vazquez
+ID:1719210003
+DATE:05/06/2019
+GROUP:TIC22
+DESCRIPTION:Calculo de area de una circuferencia
+"""
+pi=3.1416
+rad=4
+area=pi*rad*rad
+if area>10:
+	print ("Area1",area)
+elif area<10:
+	print ("Area2",area)
+print ("end")
