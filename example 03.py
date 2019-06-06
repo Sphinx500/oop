22555de320d8ee43b3063228880e90c157813901
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2019-06-06 10:51:14 -0500

    Add files via upload

---
+++ example 03.py
@@ -0,0 +1,23 @@
+"""
+NOMBRE:Fernando Hernandez Vazquez
+ID:1719210003
+DATE:06/06/2019
+GROUP:TIC22
+DESCRIPTION:Uso de for
+"""
+for i in range(10):
+	print ("Hola Mundo",i)
+numbers=["uno","dos","tres"]
+for i in numbers:
+	print ("Hola Mundo",i)
+numbers=[10.5,14,18.3]
+for i in numbers:
+	print ("Hola Mundo",(i+1))
+numbers=[10.5,14,18.23]
+for i in numbers:
+	print ((i+1))
+	print ("bye")
+numbers=[10.5,14,18.23]
+for i in numbers:
+	print ((i+1, "\nbye"))
+	
