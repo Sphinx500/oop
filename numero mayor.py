b00167f3e7f3a005b5a195f08966475f1796221a
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2019-06-11 07:56:18 -0500

    Add files via upload

---
+++ numero mayor.py
@@ -0,0 +1,16 @@
+"""
+NOMBRE:Fernando Hernandez Vazquez
+ID:1719210003
+DATE:11/06/2019
+GROUP:TIC22
+DESCRIPTION:imprime el numero mayor. V1
+"""
+n1=input()
+n2=input()
+n3=input()
+if n1>n2 and n1>n3:
+    	print ("el numero mayor es:"+str(n1))
+elif n2>n1 and n2>n3:
+     	print ("el numero mayor es:"+str(n2))
+elif n3>n1 and n3>n2:
+	print ("el numero mayor es:"+str(n3))
