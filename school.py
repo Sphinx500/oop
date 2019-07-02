2f8f6d7748dfcd5bb0003b0bd8ce4b9617280384
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2019-07-02 09:00:47 -0500

    Add files via upload

--- school.py
+++ school.py
@@ -1,11 +1,16 @@
+"""
+NOMBRE:Fernando Hernandez Vazquez
+ID:1719210003
+DATE:02/07/2019
+GROUP:TIC22
+DESCRIPTION:Clases
+"""
 class School:
     def __init__(self,name,teacher_name,id):
         self.name=name
         self.id=id
         self.teacher_name=teacher_name
-hidalgo=School("Colegio",)
+hidalgo=School("Colegio")
 print(hidalgo.name)
 fer=School("Fer","Hernandez")
-print(fer.teacher_name)
-id=School("1719210003")
-print(id.id)
+print(hidalgo.teacher_name)
