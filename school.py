fbc49992789fcf305a1eea35811fe4d331f35776
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2019-07-02 08:58:51 -0500

    Add files via upload

---
+++ school.py
@@ -0,0 +1,11 @@
+class School:
+    def __init__(self,name,teacher_name,id):
+        self.name=name
+        self.id=id
+        self.teacher_name=teacher_name
+hidalgo=School("Colegio",)
+print(hidalgo.name)
+fer=School("Fer","Hernandez")
+print(fer.teacher_name)
+id=School("1719210003")
+print(id.id)
